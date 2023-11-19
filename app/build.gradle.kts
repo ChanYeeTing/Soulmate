175baa1927757c4ead25986f8b93d10006ee0d07
ChanYeeTing <yeeting@student.usm.my>
2023-11-19 22:16:25 +0800

    fix minSDK error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.soulmate"
-        minSdk = 18
+        minSdk = 19
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
