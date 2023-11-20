c26189f802673bf3bcc5ad0ad0f01f03664f8973
ChanYeeTing <yeeting@student.usm.my>
2023-11-20 14:55:24 +0800

    add keyword

--- app/src/main/java/com/example/soulmate/MainActivity.java
+++ app/src/main/java/com/example/soulmate/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.soulmate;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseReference rootDatabaseref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootDatabaseref= FirebaseDatabase.getInstance().getReference();
+
+
+
     }
 }
