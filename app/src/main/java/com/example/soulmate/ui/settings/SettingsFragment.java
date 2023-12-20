059b0071ae26f398773a9ea59ebb4cb9b4af0f8a
WLing3216 <wailing3216@gmail.com>
2023-12-20 19:50:59 +0800

    User Profile Page + Update Profile Page

--- app/src/main/java/com/example/soulmate/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/soulmate/ui/settings/SettingsFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.soulmate.R;
 import com.example.soulmate.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -24,8 +29,6 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate ( inflater, container, false );
         View root = binding.getRoot ();
 
-        final TextView textView = binding.textSettings;
-        SettingsViewModel.getText ().observe ( getViewLifecycleOwner (), textView::setText );
         return root;
     }
 
@@ -33,5 +36,20 @@ public class SettingsFragment extends Fragment {
     public void onDestroyView () {
         super.onDestroyView ();
         binding = null;
+    }
+
+    @Override
+    public void onViewCreated ( @NonNull View view, @Nullable Bundle savedInstanceState ) {
+        super.onViewCreated ( view, savedInstanceState );
+
+        Button updateProfile = getView ().findViewById ( R.id.updateProfileButton );
+
+        updateProfile.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick ( View v ) {
+                NavController controller = Navigation.findNavController ( v );
+                controller.navigate ( R.id.action_nav_settings_to_updateProfile );
+            }
+        } );
     }
 }
