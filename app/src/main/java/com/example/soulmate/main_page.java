7cd9bcdda7d912e5da8f118d841688affa470b02
WLing3216 <wailing3216@gmail.com>
2023-12-20 16:07:56 +0800

    logout feature

--- app/src/main/java/com/example/soulmate/main_page.java
+++ app/src/main/java/com/example/soulmate/main_page.java
@@ -2,6 +2,7 @@ package com.example.soulmate;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -54,37 +56,30 @@ public class main_page extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController ( this, navController, mAppBarConfiguration );
         NavigationUI.setupWithNavController ( navigationView, navController );
 
-
-
-
-
-
-
-
-
         //Edit username
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        String currentUser = firebaseAuth.getCurrentUser().getUid();
-        NavigationView navigationView1 = (NavigationView)findViewById(R.id.nav_view);
-        View headerView = navigationView1.getHeaderView(0);
-        username = headerView.findViewById(R.id.username);
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
-        reference.child(currentUser).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    if (task.getResult().exists()) {
-                        DataSnapshot dataSnapshot = task.getResult();
-                        String name = String.valueOf(dataSnapshot.child("name").getValue());
-                        username.setText(name);
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser(); // Check for null here
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            NavigationView navigationView1 = binding.navView;
+            View headerView = navigationView1.getHeaderView(0);
+            username = headerView.findViewById(R.id.username);
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+            reference.child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().exists()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            String name = String.valueOf(dataSnapshot.child("name").getValue());
+                            username.setText(name);
+                        }
                     }
-
                 }
-            }
-        });
+            });
+        }
 
-
-    }
+        }
 
     @Override
     public boolean onCreateOptionsMenu ( Menu menu ) {
@@ -98,5 +93,30 @@ public class main_page extends AppCompatActivity {
         NavController navController = Navigation.findNavController ( this, R.id.nav_host_fragment_content_main_page );
         return NavigationUI.navigateUp ( navController, mAppBarConfiguration )
                 || super.onSupportNavigateUp ();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_sign_out) {
+            signOut();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        navigateToLoginFragment();
+    }
+
+    private void navigateToLoginFragment() {
+        // Use the view parameter to find the NavController associated with the current view hierarchy
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_page);
+
+        // Navigate to the login destination
+        navController.navigate(R.id.action_global_login);
     }
 }
