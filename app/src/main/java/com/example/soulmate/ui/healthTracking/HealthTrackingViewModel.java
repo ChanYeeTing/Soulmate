5740f39843ee8a6928cf009a1019378e9e6130e1
WLing3216 <wailing3216@gmail.com>
2023-12-23 18:41:49 +0800

    Add Health Tracking Page and Analyze Result Page

---
+++ app/src/main/java/com/example/soulmate/ui/healthTracking/HealthTrackingViewModel.java
@@ -0,0 +1,19 @@
+package com.example.soulmate.ui.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HealthTrackingViewModel extends ViewModel {
+
+    private static MutableLiveData<String> mText;
+
+    public HealthTrackingViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is health tracking fragment");
+    }
+
+    public static LiveData<String> getText() {
+        return mText;
+    }
+}
