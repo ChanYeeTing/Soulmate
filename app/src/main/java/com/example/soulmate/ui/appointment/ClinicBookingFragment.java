4667839a2713eaec206474a2624daafe08afff32
JY Beh <behjianyong@gmail.com>
2023-12-24 23:12:19 +0800

    Finished

--- app/src/main/java/com/example/soulmate/ui/appointment/ClinicBookingFragment.java
+++ app/src/main/java/com/example/soulmate/ui/appointment/ClinicBookingFragment.java
@@ -1,15 +1,46 @@
 package com.example.soulmate.ui.appointment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.soulmate.DatePickerFragment;
 import com.example.soulmate.R;
 import com.example.soulmate.main_page;
+import com.example.soulmate.timeslot;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +57,24 @@ public class ClinicBookingFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private DatePickerDialog.OnDateSetListener DateSetListener;
+    TextView Selectdate;
+    TextView name;
+    TextView number;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    String getName,getNumber;
+    public static String getDate ;
+    public static String check;
+    String getTime;
 
     public ClinicBookingFragment () {
         // Required empty public constructor
@@ -64,9 +113,304 @@ public class ClinicBookingFragment extends Fragment {
         View view = inflater.inflate ( R.layout.fragment_clinic_booking, container, false );
 
         if (getActivity() instanceof main_page) {
-            ((main_page) getActivity()).getSupportActionBar().setTitle("Clinic / Hospital Booking");
+            ((main_page) getActivity()).getSupportActionBar().setTitle("Clinic/Hospital Booking");
         }
 
+        String[] hospitalNames = {"Island Hospital", "Clinic Medicris", "Hospital Lam Wah Ee", "Clinic Dr. Dashindar Singh",
+                "Medivici Clinic & Surgery", "Clinic Putra Simpang Ampat", "Clinic Lim", "House Call Doctor", "Clinic Medilife"};
+
+        Spinner hospitalSpinner = view.findViewById(R.id.hospital_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, hospitalNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        hospitalSpinner.setAdapter(adapter);
+
+        // Map to store hospital categories
+        Map<String, List<String>> hospitalInfo = new HashMap<>();
+        // Populate the map with hospital information
+        populateHospitalInfo(hospitalInfo);
+
+        // Listener for hospital selection
+        hospitalSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedHospital = hospitalNames[position];
+                List<String> categoriesAndDoctors = hospitalInfo.get(selectedHospital);
+
+                // Now you can use the categoriesAndDoctors list to populate the category_spinner
+                // You need to set up the category_spinner in your XML layout and reference it here
+                Spinner categorySpinner = view.findViewById(R.id.category_spinner);
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, categoriesAndDoctors);
+                categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                categorySpinner.setAdapter(categoryAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing here
+            }
+        });
         return view;
+    }
+
+    private void populateHospitalInfo(Map<String, List<String>> hospitalInfo) {
+        // Populate the hospitalInfo map with hospital names as keys and categories as values
+        // You can adjust this according to your actual data
+        hospitalInfo.put("Island Hospital", List.of("Cardiology", "Cardiothoracic Surgery", "Family Medicine", "General Paediatrics", "General Surgery", "Orthodontist", "Neurology", "Neurosurgery", "Plastic Surgery"));
+        hospitalInfo.put("Clinic Medicris", List.of("Family medicine", "Advanced Wound Care Management", "Women and Child Care"));
+        hospitalInfo.put("Hospital Lam Wah Ee", List.of("Cardiology", "Cardiothoracic Surgery", "Dentistry", "Ear, Nose and Throat Surgery", "Emergency Medicine", "General Medicine", "Neurosurgery", "Respiratory Medicine", "Urology"));
+        hospitalInfo.put("Clinic Dr. Dashindar Singh", List.of("General Medicine"));
+        hospitalInfo.put("Medivici Clinic & Surgery", List.of("General Medicine", "Health Check", "Home Visit"));
+        hospitalInfo.put("Clinic Putra Simpang Ampat", List.of("Internal & Family Medicine", "Home Visit", "Medical Check Up", "Chronic illnesses"));
+        hospitalInfo.put("Clinik Lim", List.of("Medical & Dental Surgery"));
+        hospitalInfo.put("House Call Doctor", List.of("Medical Check Up", "Home Consultation", "Diabetes Care"));
+        hospitalInfo.put("Clinic Medilife", List.of("Physiotherapy and Rehab Centre", "Hemodialysis"));
+    }
+    @Override
+    public void onActivityCreated ( @Nullable Bundle savedInstanceState ) {
+        super.onActivityCreated(savedInstanceState);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser(); // Check for null here
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+
+            //set username
+            name = getView().findViewById(R.id.name);
+            number = getView().findViewById(R.id.number);
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+            reference.child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().exists()) {
+                            DataSnapshot dataSnapshot = task.getResult();
+                            String user = String.valueOf(dataSnapshot.child("name").getValue());
+                            name.setText(user);
+                            getName = user;
+                            String contact = String.valueOf(dataSnapshot.child("Mobile Number").getValue());
+                            number.setText(contact);
+                            getNumber=contact;
+                        }
+                    }
+                }
+            });
+
+        }
+
+        Selectdate= getView().findViewById(R.id.date);
+        button1= getView().findViewById(R.id.button1);
+        button2= getView().findViewById(R.id.button2);
+        button3= getView().findViewById(R.id.button3);
+        button4= getView().findViewById(R.id.button4);
+        button5= getView().findViewById(R.id.button5);
+        button6= getView().findViewById(R.id.button6);
+        button7= getView().findViewById(R.id.button7);
+        button8= getView().findViewById(R.id.button8);
+        button9= getView().findViewById(R.id.button9);
+        check = Selectdate.getText().toString().trim();
+        enableButton();
+
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableButton();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                check = Selectdate.getText().toString().trim();
+                enableButton();
+                getDate = Selectdate.getText().toString().trim();
+
+            }
+        };
+        Selectdate.addTextChangedListener(textWatcher);
+        Selectdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getActivity().getSupportFragmentManager(),"datePicker");
+
+            }
+        });
+
+
+
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        timeslot showPopUp = new timeslot();
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button1.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button2.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button3.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button4.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button5.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button6.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button7.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button8.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getTime = button9.getText().toString().trim();
+                Bundle bundle = new Bundle();
+                bundle.putString("name", getName);
+                bundle.putString("number",getNumber);
+                bundle.putString("date",getDate);
+                bundle.putString("time",getTime);
+
+                showPopUp.setArguments(bundle);
+                showPopUp.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "showpopup");
+
+            }
+        });
+
+
+    }
+    private void enableButton() {
+
+        Calendar calendar = Calendar.getInstance(
+                TimeZone.getTimeZone("Asia/Kuala_Lumpur"));;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault());
+        String todayDateMediumFormat = dateFormat.format(calendar.getTime());
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
+
+        if (check.equals(todayDateMediumFormat.trim()))
+        {
+            button1.setEnabled(!check.equals("Select Date")&&(9>currentHour));
+            button2.setEnabled(!check.equals("Select Date")&&(10>currentHour));
+            button3.setEnabled(!check.equals("Select Date")&&(11>currentHour));
+            button4.setEnabled(!check.equals("Select Date")&&(12>currentHour));
+            button5.setEnabled(!check.equals("Select Date")&&(14>currentHour));
+            button6.setEnabled(!check.equals("Select Date")&&(15>currentHour));
+            button7.setEnabled(!check.equals("Select Date")&&(16>currentHour));
+            button8.setEnabled(!check.equals("Select Date")&&(17>currentHour));
+            button9.setEnabled(!check.equals("Select Date")&&(18>currentHour));
+        }
+        else {
+            button1.setEnabled(!check.equals("Select Date"));
+            button2.setEnabled(!check.equals("Select Date"));
+            button3.setEnabled(!check.equals("Select Date"));
+            button4.setEnabled(!check.equals("Select Date"));
+            button5.setEnabled(!check.equals("Select Date"));
+            button6.setEnabled(!check.equals("Select Date"));
+            button7.setEnabled(!check.equals("Select Date"));
+            button8.setEnabled(!check.equals("Select Date"));
+            button9.setEnabled(!check.equals("Select Date"));
+        }
+
     }
 }
