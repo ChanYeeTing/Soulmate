1a1df07f9b4dec415ca2a522aeb1132cdb9f8ec6
ChanYeeTing <yeeting@student.usm.my>
2024-01-05 13:17:48 +0800

    fix bug - name in side bar not change after updated

--- app/src/main/java/com/example/soulmate/main_page.java
+++ app/src/main/java/com/example/soulmate/main_page.java
@@ -15,14 +15,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.soulmate.databinding.ActivityMainPageBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class main_page extends AppCompatActivity {
 
@@ -31,12 +31,12 @@ public class main_page extends AppCompatActivity {
     TextView username;
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
-        super.onCreate ( savedInstanceState );
+        super.onCreate(savedInstanceState);
 
-        binding = ActivityMainPageBinding.inflate ( getLayoutInflater () );
-        setContentView ( binding.getRoot () );
+        binding = ActivityMainPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setSupportActionBar ( binding.appBarMainPage.toolbar );
+        setSupportActionBar(binding.appBarMainPage.toolbar);
 /*        binding.appBarMainPage.fab.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick ( View view ) {
@@ -48,13 +48,13 @@ public class main_page extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder (
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_medical_history, R.id.nav_settings, R.id.nav_telemedicine, R.id.nav_date_tracking)
-                .setOpenableLayout ( drawer )
-                .build ();
-        NavController navController = Navigation.findNavController ( this, R.id.nav_host_fragment_content_main_page );
-        NavigationUI.setupActionBarWithNavController ( this, navController, mAppBarConfiguration );
-        NavigationUI.setupWithNavController ( navigationView, navController );
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_page);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
         //Edit username
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
@@ -66,21 +66,24 @@ public class main_page extends AppCompatActivity {
 
             //set username
             username = headerView.findViewById(R.id.username);
-            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
-            reference.child(uid).child("User Info").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(uid).child("User Info");
+            reference.addValueEventListener(new ValueEventListener() {
+
                 @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DataSnapshot dataSnapshot = task.getResult();
-                        if (dataSnapshot != null && dataSnapshot.exists()) {
-                            String name = String.valueOf(dataSnapshot.child("name").getValue());
-                            username.setText(name);
-                        }
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot != null && snapshot.exists()) {
+                        String name = String.valueOf(snapshot.child("name").getValue());
+                        username.setText(name);
                     }
                 }
-            });
-        }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
     }
 
     @Override
