d20cb019a14d001639338c9f816a7a9caca25aa8
WLing3216 <wailing3216@gmail.com>
2024-01-14 18:01:37 +0800

    logout feature (teledoctor main page)

--- app/src/main/java/com/example/soulmate/TeleDoctorMainPageFragment.java
+++ app/src/main/java/com/example/soulmate/TeleDoctorMainPageFragment.java
@@ -2,11 +2,18 @@ package com.example.soulmate;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +67,23 @@ public class TeleDoctorMainPageFragment extends Fragment {
                                Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         return inflater.inflate ( R.layout.fragment_tele_doctor_main_page, container, false );
+    }
+
+    @Override
+    public void onViewCreated ( @NonNull View view, @Nullable Bundle savedInstanceState ) {
+        super.onViewCreated ( view, savedInstanceState );
+
+        Button logoutButton = view.findViewById(R.id.logoutButton4);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sign out the user from Firebase Authentication
+                FirebaseAuth.getInstance().signOut();
+
+                // Navigate to the login fragment
+                NavController controller = Navigation.findNavController(v);
+                controller.navigate(R.id.action_teleDoctorMainPageFragment_to_teleDoctorLoginFragment);
+            }
+        });
     }
 }
