e3c16a77a1da0c004077cacf05077383d4c2fb74
ChanYeeTing <yeeting@student.usm.my>
2024-01-20 21:26:55 +0800

    pass hospital name to each fragment and solve the button problem

---
+++ app/src/main/java/com/example/soulmate/SharedViewModel.java
@@ -0,0 +1,21 @@
+package com.example.soulmate;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedViewModel extends ViewModel {
+    private final MutableLiveData<String> sharedData = new MutableLiveData<>();
+
+    public void setSharedData(String data) {
+        sharedData.setValue(data);
+    }
+
+    public LiveData<String> getSharedData() {
+        return sharedData;
+    }
+
+    public String getData() {
+        return sharedData.getValue().toString();
+    }
+}
